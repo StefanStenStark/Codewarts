3f5a651405ae44a8eaa8c22bfbf1755e78e4884d
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-16 16:40:44 +0200

    feat: swagger online all the time

--- CWback/Program.cs
+++ CWback/Program.cs
@@ -14,11 +14,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
