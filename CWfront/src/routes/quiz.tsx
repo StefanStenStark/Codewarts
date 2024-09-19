bf0825616e860106da7b3ad320e1a189e8e4b5ae
abedfetrat <abedfetrat@gmail.com>
2024-09-19 15:01:15 +0200

    feat: improve quiz flow and add health bar

--- CWfront/src/routes/quiz.tsx
+++ CWfront/src/routes/quiz.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { Question, Option } from "../data/types";
+import {createFileRoute} from "@tanstack/react-router";
+import {useState} from "react";
+import {Question, Option} from "../data/types";
+import {fetchUser} from '../data/Api';
 
 const questions: Question[] = [
   {
@@ -37,38 +38,56 @@ const questions: Question[] = [
   },
 ];
 export const Route = createFileRoute("/quiz")({
-  component: () => <Quiz />,
+  component: () => <Quiz/>,
 });
 
 function Quiz() {
+  const user = fetchUser();
   const [currentQuestion, setCurrentQuestion] = useState<Question>(
     questions[0]
   );
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-  const [submitted, setSubmitted] = useState(false);
-
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [heartsCount, setHeartsCount] = useState(user.maximumHearts);
+  
   const questionIndex = questions.indexOf(currentQuestion);
   const questionNumber = questionIndex + 1;
   const isLastQuestion = questionNumber === questions.length;
   const progress = (questionNumber / questions.length) * 100;
 
   const handleSubmit = () => {
-    if (submitted) {
+    if (selectedOption!.isCorrect) {
+      if (isLastQuestion) {
+        handleFinish();
+        return;
+      }
+      // TODO: Increase user XP
       const nextQuestion = questions[questionIndex + 1];
       setCurrentQuestion(nextQuestion);
       setSelectedOption(null);
-      setSubmitted(false);
+      setIsSubmitted(false);
     } else {
-      setSubmitted(true);
+      // TODO: lose health
+      if (heartsCount === 1) {
+        // TODO: handle retry
+        return;
+      }
+      setIsSubmitted(true);
+      setHeartsCount(prev => prev - 1);
     }
   };
 
   const handleFinish = () => {
     // TOOD: Finish the quiz
+    alert("Quiz finished!");
   };
 
   return (
     <main className="grid place-items-center h-screen p-6">
+      <div className="flex gap-4">
+        {Array(heartsCount).fill(0).map(_ => <HeartIcon/>)}
+      </div>
+
       <section className="max-w-2xl bg-base-200 rounded-3xl p-12">
         <div>
           <span className="text-sm">
@@ -85,45 +104,48 @@ function Quiz() {
         </h2>
         <fieldset
           name="options"
-          className="flex flex-col gap-3 mt-12"
-          disabled={submitted}
-        >
+          className="flex flex-col gap-3 mt-12">
           {currentQuestion.options.map((opt) => (
             <div key={opt.id} className="form-control">
               <label
-                className={`label cursor-pointer justify-start gap-8 bg-base-300 rounded-lg p-4 border-secondary has-[:checked]:border ${submitted && (opt.isCorrect ? "border border-success" : "border border-error")}`}
-              >
+                className={`label cursor-pointer justify-start gap-8 bg-base-300 rounded-lg p-4 has-[:checked]:border ${isSubmitted ? ((opt.id === selectedOption!.id) && "border-error") : "border-secondary"}`}>
                 <input
                   type="radio"
                   name="option"
-                  className={`radio radio-secondary disabled:opacity-100 ${submitted && (opt.isCorrect ? "radio-success" : "radio-error")}`}
+                  className={`radio radio-secondary disabled:opacity-100 ${isSubmitted && (opt.id == selectedOption!.id) && "radio-error"}`}
                   checked={
                     selectedOption ? selectedOption.id === opt.id : false
                   }
-                  onChange={() => setSelectedOption(opt)}
+                  onChange={() => {
+                    setIsSubmitted(false);
+                    setSelectedOption(opt)
+                  }}
                 />
                 <span className="label-text font-mono">{opt.optionText}</span>
               </label>
             </div>
           ))}
         </fieldset>
-        {submitted && isLastQuestion ? (
-          <button
-            onClick={handleFinish}
-            className="btn btn-primary btn-block mt-14"
-          >
-            Finish
-          </button>
-        ) : (
-          <button
-            disabled={!selectedOption}
-            onClick={handleSubmit}
-            className="btn btn-primary btn-block mt-14"
-          >
-            {submitted ? "Next" : "Submit"}
-          </button>
-        )}
+        <button
+          disabled={!selectedOption}
+          onClick={handleSubmit}
+          className="btn btn-primary btn-block mt-14">
+          Submit
+        </button>
       </section>
     </main>
   );
 }
+
+const HeartIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="fill-error"
+    width="48"
+    height="48"
+    viewBox="0 0 512 512">
+    <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68
+    47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244
+    84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+  </svg>
+);
