8792429db206fcfabee6d58ab809dcd5375b059e
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-19 15:07:15 +0200

    feat: HTTP PUT Endpoint for Update User

--- CWback/Models/Option.cs
+++ CWback/Models/Option.cs
@@ -3,6 +3,6 @@ namespace CWback.Models;
 public class Option
 {
     public int Id { get; set; }
-    public string OptionText { get; set; }
+    public string? OptionText { get; set; }
     public bool IsCorrect { get; set; }
 }
