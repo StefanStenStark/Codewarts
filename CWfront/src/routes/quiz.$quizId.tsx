d562b3b2ba2593a77c252cb4005d45c5b6d3b0f5
abedfetrat <abedfetrat@gmail.com>
2024-09-22 17:40:22 +0200

    feat: get temp questions from api

--- CWfront/src/routes/quiz.$quizId.tsx
+++ CWfront/src/routes/quiz.$quizId.tsx
@@ -1,49 +1,16 @@
 import {createFileRoute, useNavigate} from "@tanstack/react-router";
 import {useState} from "react";
-import {Question } from "../data/types";
-import {fetchUser} from "../data/Api";
+import {fetchTempQuestions, fetchUser} from "../data/Api";
 import HealthBar from "../components/quiz/HealthBar.tsx";
 import SingleChoiceQuiz from "../components/quiz/SingleChoiceQuiz.tsx";
 
-const questions: Question[] = [
-  {
-    id: 1,
-    questionText: "Which is the correct syntax for string interpolation?",
-    options: [
-      {
-        id: 1,
-        optionText: '$"Hello, World"',
-        isCorrect: true,
-      },
-      {
-        id: 2,
-        optionText: '$"Hello, There"',
-        isCorrect: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    questionText: "Which is the correct syntax for something?",
-    options: [
-      {
-        id: 1,
-        optionText: '$"Hello, World"',
-        isCorrect: false,
-      },
-      {
-        id: 2,
-        optionText: '$"Hello, There"',
-        isCorrect: true,
-      },
-    ],
-  },
-];
 export const Route = createFileRoute("/quiz/$quizId")({
   component: () => <Quiz/>,
+  loader: () => fetchTempQuestions()
 });
 
 function Quiz() {
+  const questions = Route.useLoaderData();
   const {quizId} = Route.useParams();
   const navigate = useNavigate();
   const user = fetchUser();
