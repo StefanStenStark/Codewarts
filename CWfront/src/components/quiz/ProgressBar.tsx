5e48cfb5ecdedca160527d12b278e05b63719970
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-24 17:37:35 +0200

    fix: updated question component styling

--- CWfront/src/components/quiz/ProgressBar.tsx
+++ CWfront/src/components/quiz/ProgressBar.tsx
@@ -1,13 +1,19 @@
-export default function ProgressBar({currQuestion, totalQuestions}: { currQuestion: number, totalQuestions: number }) {
+export default function ProgressBar({
+  currQuestion,
+  totalQuestions,
+}: {
+  currQuestion: number;
+  totalQuestions: number;
+}) {
   const progress = (currQuestion / totalQuestions) * 100;
 
   return (
     <div>
-        <span className="text-sm">
-          Question {currQuestion} of {totalQuestions}
-        </span>
+      <span className="text-sm font-mono">
+        Question {currQuestion} of {totalQuestions}
+      </span>
       <progress
-        className="progress progress-secondary"
+        className="progress progress-error"
         value={progress}
         max="100"
       ></progress>
