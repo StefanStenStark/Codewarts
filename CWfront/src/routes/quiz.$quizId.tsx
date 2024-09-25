9501375ada8d9e20be8b8b79217e0fd01bb790ea
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-25 15:04:31 +0200

    fix: remove double variable declaration

--- CWfront/src/routes/quiz.$quizId.tsx
+++ CWfront/src/routes/quiz.$quizId.tsx
@@ -95,7 +95,6 @@ function Quiz() {
     async function getUser() {
       setLoading(true);
       const fetchedUser = await fetchUser(5);
-      const fetchedUser = await fetchUser(5);
       setUser(fetchedUser);
       setLoading(false);
     }
