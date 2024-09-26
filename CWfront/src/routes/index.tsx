95c34ac3f233d27c5fea14b8bd19021e2146db6a
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-09-26 16:08:42 +0200

    feat: button size fixed on logout

--- CWfront/src/routes/index.tsx
+++ CWfront/src/routes/index.tsx
@@ -28,7 +28,7 @@ function Index() {
       <section className="relative flex justify-center items-center bg-center mt-12">
         <div className="relative w-4/5 p-10 bg-base-100 border-2 shadow-md">
           <SignedIn>
-            <div className="flex flex-col items-center space-y-4">
+            <div className="flex flex-col items-center space-y-4 m-8">
               <p className="text-center text-xl text-amber-300 font-serif p-4">
                 Ready for another adventure?
               </p>
@@ -41,18 +41,23 @@ function Index() {
 
           <SignedOut>
             <div className="flex flex-col items-center space-y-4 m-8">
-              <p className="text-center text-xl text-amber-300 font-serif p-2">
+              <p className="text-center text-xl text-amber-300 font-serif p-4">
                 Please log in or try an adventure!
               </p>
-              <div>
+              <div className="">
                 <SignInButton mode="modal">
-                  <button className="m-4 bg-[rgba(255,255,255,0.11)] hover:bg-[rgb(90,114,118)] h-16 text-white font-bold py-2 px-8 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 flex justify-center items-center border border-white">
+                  <button className="m-4 bg-[rgba(255,255,255,0.11)]  h-16 text-white font-bold py-2 px-8 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 flex justify-center items-center border border-white rounded-md">
                     Sign In/ sign up
                   </button>
                 </SignInButton>
               </div>
 
-              <GoToAdventureButton linkTo="/adventures" text="Test adventure" />
+              <div className="flex flex-col gap-4">
+                <GoToAdventureButton
+                  linkTo="/adventures"
+                  text="Test adventure"
+                />
+              </div>
             </div>
           </SignedOut>
         </div>
