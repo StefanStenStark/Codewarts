44ad6288153a94a92fe5bd8cef11d32969c2e075
abedfetrat <abedfetrat@gmail.com>
2024-09-27 11:25:56 +0200

    chore: remove tanstack devtools

--- CWfront/src/routes/__root.tsx
+++ CWfront/src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import BlinkingLogo from "../components/BlinkLogo";
 
 export const Route = createRootRoute({
@@ -16,7 +15,7 @@ export const Route = createRootRoute({
         <Outlet />
       </main>
 
-      <TanStackRouterDevtools />
+      {/*<TanStackRouterDevtools />*/}
     </>
   ),
 });
